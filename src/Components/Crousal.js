eff79c569990b9dbdf4bce7fe15ce007551c1220
Manjunathmj0510 <manjunathdhanabal@gmail.com>
2022-06-03 03:00:38 +0530

    task

--- src/Components/Crousal.js
+++ src/Components/Crousal.js
@@ -10,6 +10,27 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import {FaChevronLeft, FaChevronRight} from 'react-icons/fa'
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#555",borderRadius:'50%' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block",  background: "#555",borderRadius:'50%'}}
+      onClick={onClick}
+    />
+  );
+}
 export default class Carousel extends Component {
   render() {
     const settings = {
@@ -18,6 +39,8 @@ export default class Carousel extends Component {
       speed: 500,
       slidesToShow: 4,
       slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
       responsive: [
         {
           breakpoint: 1024,
@@ -70,6 +93,32 @@ export default class Carousel extends Component {
                 description: 'We give entrepreneurs access to our technology, expertise and networks, helping them build the global businesses of tomorrow.',
                 pricingText: 'USD 150/Day',
                 features: ['Free Wifi', 'Free breakfast', 'Discounted Meals'],
+              },
+              {
+                imageSrc:
+                  'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                title: 'Royal Suite',
+                description: 'We give entrepreneurs access to our technology, expertise and networks, helping them build the global businesses of tomorrow.',
+                pricingText: 'USD 299/Day',
+                features: [
+                  'Free Wifi',
+                  'Free breakfast',
+                  'Discounted Meals',
+                  "MacBook for work use (hotel's property)",
+                ],
+              },
+              {
+                imageSrc:
+                  'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                title: 'Royal Suite',
+                description: 'We give entrepreneurs access to our technology, expertise and networks, helping them build the global businesses of tomorrow.',
+                pricingText: 'USD 299/Day',
+                features: [
+                  'Free Wifi',
+                  'Free breakfast',
+                  'Discounted Meals',
+                  "MacBook for work use (hotel's property)",
+                ],
               },
               {
                 imageSrc:
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Carousel from './Crousal';
 import './Home.css'
-import {AiFillTwitterCircle} from 'react-icons/ai'
+import {AiFillTwitterCircle,AiOutlineCopyright} from 'react-icons/ai'
 import {FaFacebook,FaLinkedin} from 'react-icons/fa'
 import {RiInstagramLine} from 'react-icons/ri'
+import {IoIosArrowForward} from 'react-icons/io'
 
 function Home(props) {
     return (
-        <div className='home-container'>
+        <div className='home-container-section'>
         <section className='header-section'>
-        <div className='header-brand'><h3 className='header-brand-text'>Innovation</h3></div>
+        <div className='header-brand'><h3 className='header-brand-text'>INNOVATION</h3></div>
         <div className='header-items-section'>
         <ul className='header-items'>
         <li>Aboutus</li>
@@ -21,27 +22,33 @@ function Home(props) {
         </ul>
         </div>
         </section>
+        <div className='home-container'>
         <section className='content-section'>
         <div className='content-wrapper'>
         <div className='content-div'>
         <p className='content-highlight'><small className='content-line'>challenging limits everyday</small><br/>
         Scale.Innovate.<br/>Impact the Future.</p>
         <p className='content-para'>We collaborate with start-ups and external agencies to address<br/> challenges in the steel value chain and proximate sectors.</p>
-        <button className='header-button'>Apply Now</button>
+        <button className='content-button'>Apply Now<IoIosArrowForward/></button>
         </div><div className='image-div'>
-        <img className='image-div-image' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUK1LaBaummJfuW6GIM_kt3R9egIlpqVpEKw&usqp=CAU' alt='dummy'/>
-        <img className='image-div-image2' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUK1LaBaummJfuW6GIM_kt3R9egIlpqVpEKw&usqp=CAU' alt='dummy'/>
+        <img className='image-div-image' src='https://th.bing.com/th/id/OIP.9gmmHhAGK7Q93IezLHhAhgHaEo?w=304&h=190&c=7&r=0&o=5&pid=1.7' alt='dummy'/>
+        <img className='image-div-image1' src='https://th.bing.com/th/id/OIP.RP-HEWi3BhaXoW_cUxs-WQHaE8?w=285&h=190&c=7&r=0&o=5&pid=1.7' alt='dummy'/>
+        <img className='image-div-image2' src='https://th.bing.com/th/id/OIP.rrIexnGdFPT4c3MHAzoFgwHaE8?w=250&h=180&c=7&r=0&o=5&pid=1.7' alt='dummy'/>
         </div>
         </div>
         </section> 
         <section className='content-details-section'>
         <div className='content-details-header'>
-        <p className='content-highlight'><small className='content-line'>Lead the Revolution</small><br/>
-        Your Vision.Our Network.<br/>Let's build a legacy togeter.</p></div>
+        <p className='content-highlight1'><small className='content-line'>Lead the Revolution</small><br/>
+        Your Vision.Our Network.<br/>Let's build a legacy togeter.</p>
+        <button className='content-button'>Learn About Us<IoIosArrowForward/></button></div>
         </section>
-        <section>
-            <Carousel/>
+        <section className='crousel-section'>
+        <div className='crousel-section-container'>
+        <Carousel/>
+        </div>
         </section>
+        </div>
         <section className='footer-section'>
             <div className='home-footer'>
               <div className='footer-brand'>INNOVATION</div>
@@ -54,6 +61,7 @@ function Home(props) {
         <li>News & Events</li>
               </ul></div>
             </div>
+            <div className='social-wrapper'>
             <div className='social-icons'>
                 <p>contact us on</p>
                 <ul className='icons-list'>
@@ -63,6 +71,8 @@ function Home(props) {
                 <li><RiInstagramLine/></li>
                 </ul>
             </div>
+            <div className='terms-list'><ul className='terms-list-itmes'><li>Terms & Contions</li><li><AiOutlineCopyright/>&nbsp;Copy Rights</li></ul></div>
+        </div>
         </section>   
         </div>
     );
